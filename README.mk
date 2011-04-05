7bd142249fe7cee523c17dae005555f99fe3c3d1
Manatsawin Hanmongkolchai <manatsawin@gmail.com>
2011-04-05 23:14:05 +0700

    Readme

---
+++ README.mk
@@ -0,0 +1,10 @@
+Noke
+===========================
+Noke name comes from NOde and karaOKE.
+I'm trying to implement a karaoke client in Node.js with Socket.io.
+
+Why?
+================
+- Almost ALL existing NCN karaoke have exactly same format
+- I can't code desktop application
+- I think I have some cool idea that need to be implemented.
