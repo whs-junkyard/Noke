6fc1a5bffc9094beea6f3abf995f4827519d0247
Manatsawin Hanmongkolchai <manatsawin@gmail.com>
2011-04-09 15:11:00 +0700

    Queue view

--- src/main.js
+++ src/main.js
@@ -63,7 +63,7 @@ app.get('/:file.lyr', function(req, res){
 	function foundCb(f){
 		fs.readFile(f, function (err, data) {
 			if (err) return;
-			conv = new Iconv('TIS-620', 'UTF-8');
+			conv = new Iconv('TIS-620', 'UTF-8//IGNORE');
 			res.send(conv.convert(data));
 		});
 	}
@@ -170,7 +170,7 @@ function finder(query){
 	result = [];
 	totalFound = 0;
 	resultType = [[],[],[]];
-	searchNum = query.match(/^[0-9]+$/);
+	searchNum = query.match(/^[a-zA-Z0-9]+$/);
 	function formatData(d){
 		// push to cache too
 		cache[path.basename(d[0]).toLowerCase()] = d[0];
@@ -185,6 +185,9 @@ function finder(query){
 	}
 	for(var d in file){
 		d = file[d];
+		if(searchNum && path.basename(d[0]).replace(/\.lyr$/i, "") == query){
+			return [formatData(d)];
+		}
 		if(searchNum && path.basename(d[0]).replace(/\.lyr$/i, "").indexOf(query) == 0){
 			totalFound++;
 			result.push(formatData(d));
@@ -228,7 +231,9 @@ socket.on('connection', function(client){
 			if(!d.name.match(/^([0-9A-Z]+)$/)){
 				return;
 			}
-			queue.push(d.name);
+			ds = finder(d.name);
+			if(!ds) return;
+			queue.push(ds[0]);
 		}else if(d.type == "mute"){
 			midicore.mute(parseInt(d.channel)-1, function(){
 				channelPoller(false);
@@ -278,13 +283,14 @@ function midiPoller(){
 		d['queue'] = queue;
 		socket.broadcast({"type": "song", "data": d});
 		if(d.isplay){
-			midicore.send(["time", "key", "ctick", "mxtick", "resolution"], function(d){
+			midicore.send(["cpitch", "bpm", "ctick", "mxtick", "resolution"], function(d){
 				socket.broadcast({"type": "song", "data": d});
 				setTimeout(midiPoller, d.resolution);
 			});
 		}else{
 			if(queue.length > 0){
-				song = queue.shift();
+				songData = queue.shift();
+				song = songData.code;
 				// find
 				function foundCb(f){
 					midicore.send(["s", "sq "+f, "p"]);
